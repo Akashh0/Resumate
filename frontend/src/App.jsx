c546425979a32b584f2387707b509f6bf2a0e90a
Your Name <you@example.com>
2025-07-16 19:38:01 +0530

    Messing around

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import AuthProvider from './components/context/AuthContext';
 import ResumeUpload from './components/ResumeUpload';
+import Title from './Title';
 
 export default function App() {
   return (
     <>
     <AuthProvider>
       <NavBar />
-      <div style={{ padding: '2rem', display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
-          <h1>Welcome to Resumate</h1>
-          <p>Your AI-powered Resume companion, Feel free to use the website! Upload your Resume below to start!</p>
-      </div>
+      <Title />
     </AuthProvider>
     <ResumeUpload />
     </>  
---
+++ frontend/src/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Title.css'
+function Title() {
+  return (
+    <div className='title-text'>
+      <h1>Resume Analyzer</h1>
+      <p>Upload your resume and get a detailed analysis of your skills and experience.</p>
+    </div>
+  )
+}
+
+export default Title
